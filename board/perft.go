99fc6cda58d376ce4c522e3547e0fa184f8ead4f
connorryanbaker <connorbaker@gmail.com>
2024-03-08 22:35:15 -0500

    queenside castle bugfix

--- board/perft.go
+++ board/perft.go
@@ -1,9 +1,5 @@
 package board
 
-// import (
-//    "fmt"
-// )
-
 func perft(b *Board, depth int) uint64 {
 	var nodes uint64
 	if depth == 0 {
@@ -25,23 +21,8 @@ func BBperft(b *Board, depth int) uint64 {
 	}
 
 	bbm := b.GenerateBitboardMoves()
-	// lm := b.LegalMoves()
-	// if len(bbm) != len(lm) {
-	//   fmt.Println("ERR!", len(lm), len(bbm))
-	//   b.Print()
-	//   fmt.Println("BBM")
-	//   for _, m := range bbm {
-	//     m.Print()
-	//   }
-	//   fmt.Println("LM")
-	//   for _, m := range lm {
-	//     m.Print()
-	//   }
-	//   return nodes
-	// }
-
 	for _, m := range bbm {
-		b.MakeBBMove(m)
+		b.MakeMove(m)
 		nodes += BBperft(b, depth-1)
 		b.UnmakeMove()
 	}
